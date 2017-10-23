f81dae06bcaa0c6d0e382a062118b1145e26220d
han8909227 <hbao2016@hotmail.com>
2017-10-23 15:25:48 -0700

    working

--- client.py
+++ client.py
@@ -7,11 +7,11 @@ import sys
 
 def client_socket(message):
     """Function to send messages to client."""
-    client = socket.socket(*socket.getaddrinfo('127.0.0.1', 5000)[1][:3])
-    client.connect(('127.0.0.1', 5000))
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(('127.0.0.1', 4000))
     message = u'{}'.format(message)
     client.sendall(message.encode('utf8'))
-    buffer_length = 8
+    buffer_length = 1024
     reply_complete = False
     while not reply_complete:
         part = client.recv(buffer_length)
@@ -20,6 +20,8 @@ def client_socket(message):
             break
     client.close()
 
-
 if __name__ == '__main__':
-    sys.stdout.write(client_socket(sys.argv[1]))
+    client_socket(sys.argv[1])
+
+
+
--- server.py
+++ server.py
@@ -5,21 +5,21 @@ import socket
 def server():
     """."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    server.bind(('127.0.0.1', 5000))
-    server.listen(10)
+    server.bind(('127.0.0.1', 4000))
+    server.listen(1)
 
     while True:
         conn, addr = server.accept()
         try:
-            data = server.recv(500)
+            data = conn.recv(10)
             if data:
-                # server.sendall(data)
+                conn.sendall(data)
                 print(data)
             else:
-                server.sendall('enter valid data')
+                conn.sendall('enter valid data')
         except KeyboardInterrupt:
-            server.shutdown()
-            server.close()
+            conn.shutdown()
+            conn.close()
             break
 
 if __name__ == '__main__':
@@ -27,5 +27,4 @@ if __name__ == '__main__':
         server()
     except KeyboardInterrupt:
         pass
-
 
