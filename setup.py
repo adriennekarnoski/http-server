58caea7f355974159ad75a56bbcd89cacabc687a
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-10-27 17:40:32 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author='Han Bao and Adrienne Karnoski ',
     author_email='hbao2016@hotmail.com',
     description='http-server from ground up',
-    install_requires=[],
+    install_requires=['gevent==1.1.0', 'greenlet==0.4.9'],
     extras_require={'test': ['pytest', 'pytest-watch', 'pytest-cov', 'tox']},
 )
