b9c79ab791c24d5c1c87a811c9d2ffca96b78e7c
han8909227 <hbao2016@hotmail.com>
2017-10-28 16:07:49 -0700

    locally saved

--- src/server.py
+++ src/server.py
@@ -4,27 +4,28 @@ import email.utils
 
 
 def server():
-    """."""
+    """Main server function."""
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     server.bind(('127.0.0.1', 2200))
     server.listen(1)
     msg = ''
     buffer_len = 8
     ending = False
-    conn, addr = server.accept()
 
-    while not ending:
+    while True:
         try:
-            data = (conn.recv(buffer_len)).decode('utf8')
-            msg += data
-            if data.endswith('*'):
-                # conn.sendall(msg.encode('utf8'))
-                message_return = response_ok()
-                conn.send(message_return.encode('utf8'))
-                print(message_return)
-                print(msg)
-                msg = ''
-                break
+            conn, addr = server.accept()
+            while not ending:
+                data = (conn.recv(buffer_len)).decode('utf8')
+                msg += data
+                if data.endswith('*'):
+                    # conn.sendall(msg.encode('utf8'))
+                    message_return = response_ok()
+                    conn.send(message_return.encode('utf8'))
+                    print(message_return)
+                    print(msg)
+                    msg = ''
+                    break
         except IndexError:
             error_message = response_error()
             conn.send(error_message.encode('utf8'))
@@ -36,7 +37,7 @@ def server():
 
 
 def response_ok():
-    """."""
+    """Format a response based on current date and time."""
     send_ok_response = """
     HTTP/1.1 200 OK<CRLF>
     DATE: {}<CRLF>
@@ -46,7 +47,7 @@ def response_ok():
 
 
 def response_error():
-    """."""
+    """Format an error message based on current date and time."""
     send_error_response = """
     HTTP/1.1 500 INTERNAL SERVER ERROR<CRLF>
      DATE: {}<CRLF>
--- src/test_servers.py
+++ src/test_servers.py
@@ -2,24 +2,23 @@
 
 
 def test_response_message_response():
-    """."""
+    """Test if ok message can be formed."""
     from server import response_ok
     ok_message = response_ok()
     assert ok_message[5:20] == 'HTTP/1.1 200 OK'
 
 
 def test_error_message_response():
-    """."""
+    """Test if error message can be formed."""
     from server import response_error
     error_message = response_error()
     assert error_message[5:39] == 'HTTP/1.1 500 INTERNAL SERVER ERROR'
 
 
 def test_client_response():
-    """."""
+    """Test if a ok response can be send back."""
     from server import response_ok
     from client import client_socket
     returned = client_socket('message testing')
     ok_message = response_ok()
     assert returned == ok_message
-
