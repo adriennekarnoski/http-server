038a782d574ca3e79ed120615b413d0ef7fbe657
HAN BAO <hbao2016@hotmail.com>
2017-10-28 15:19:35 -0700

    Update test_servers.py

--- src/test_servers.py
+++ src/test_servers.py
@@ -1,22 +1,20 @@
-import pytest
-
 
 def test_shorter():
-    """."""
+    """Test for shorter than buffer length msg is echo back."""
     from client import client_socket
     msg = client_socket('12345')
     assert msg == '12345*'
 
 
 def test_longer():
-    """."""
+    """Test for longer than buffer length msg is echoed back."""
     from client import client_socket
     msg = client_socket('12345678910')
-    assert msg == '12345678\910*'
+    assert msg == '12345678910*'
 
 
 def test_exact():
-    """."""
+    """Test for exact buffer length msg is echoed back."""
     from client import client_socket
     msg = client_socket('12345678')
     assert msg == '12345678*'
